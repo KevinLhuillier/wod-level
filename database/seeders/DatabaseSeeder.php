4904079ad275a10c768f501bed841279bb49d7a6
Kevin <kevin.lhuillier@outlook.fr>
2025-05-16 08:33:07 +0200

    DB Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+
+
+        $this->call(SkillsTableSeeder::class);
+        $this->call(XpLevelTableSeeder::class);
+        $this->call(XpStageTableSeeder::class);
+        $this->call(SkillsStagesTableSeeder::class);
     }
 }
